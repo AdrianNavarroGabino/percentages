aa80438c617306172e1c7eb794ce28084002910a
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2023-01-18 22:50:04 +0100

    Consider decimal numbers

--- src/home/ConceptItem.js
+++ src/home/ConceptItem.js
@@ -6,6 +6,10 @@ export const ConceptItem = ({ concept, amount, sumPercentages }) => {
     const [result, setResult] = useState(0);
 
     const onChangePercentage = event => {
+        if (event.target.value[event.target.value.length - 1] === '.') {
+            setPercentage(event.target.value);
+            return;
+        }
         setPercentage(Number(event.target.value));
         sumPercentages(concept, Number(event.target.value));
     }
@@ -18,8 +22,8 @@ export const ConceptItem = ({ concept, amount, sumPercentages }) => {
     return (
         <div className='mt-50 conceptContainer'>
             <span className='conceptSpan'>{concept}</span>
-            <Slider className='slider' value={percentage} onChange={onChangePercentage} aria-label="Default" valueLabelDisplay="auto" max={150} sx={{ width: '300px' }} />
-            <TextField id="standard-basic" variant="standard" className='percentageSpan' type='number' value={percentage} onChange={onChangePercentage} />
+            <Slider className='slider' value={Number(percentage)} onChange={onChangePercentage} aria-label="Default" valueLabelDisplay="auto" max={150} sx={{ width: '300px' }} />
+            <TextField id="standard-basic" variant="standard" className='percentageSpan' value={percentage} onChange={onChangePercentage} />
             <TextField id="standard-basic" variant="standard" className='amountSpan' value={result} />
         </div>
     );
